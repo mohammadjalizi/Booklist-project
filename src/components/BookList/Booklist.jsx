96e6b925dad207f22d536ce28d11ce75eb135ec3
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-04 15:40:29 +0330

    added project

---
+++ src/components/BookList/Booklist.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+export default class Booklist extends Component {
+
+    constructor(props){
+super(props)
+
+
+this.state={
+
+books:[],
+titile:'',
+author: '',
+year: '',
+
+
+}
+
+
+    }
+  render() {
+    return (
+   <>
+  <form id="book-form" autocomplete="off" onSubmit={this.submitHandler}>
+                    <div className="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" id="title" className="form-control" onChange={this.titleHandler} value={this.state.title} />
+                    </div>
+
+                    <div className="form-group">
+                        <label for="author">Author</label>
+                        <input type="text" id="author" className="form-control" onChange={this.authorHandler} value={this.state.author} />
+                    </div>
+
+                    <div className="form-group">
+                        <label for="year">Year</label>
+                        <input type="text" id="year" className="form-control" onChange={this.yearHandler} value={this.state.year} />
+                    </div>
+                    <input type="submit" value="Add Book" className="btn btn-warning btn-block add-btn" />
+                </form>
+                <table class="table table-striped mt-5 text-center">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Year</th>
+                        </tr>
+                    </thead>
+                    <tbody id="book-list">
+                        {this.state.books.map(book => (
+                            <Book {...book} key={book.id} />
+                        ))}
+                    </tbody>
+                </table>
+
+
+   
+   
+   
+   </>
+    )
+  }
+}
