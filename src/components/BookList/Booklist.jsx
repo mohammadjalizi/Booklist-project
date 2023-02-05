ca73b2c2e07fd9373169052cd8da42a52aac3e21
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-05 22:09:56 +0330

    added project

--- src/components/BookList/Booklist.jsx
+++ src/components/BookList/Booklist.jsx
@@ -77,7 +77,7 @@ g
                     </thead>
                     <tbody id="book-list">
                       <book/>
-                      <P>f;djskdfjlsfj</P>
+                      <P></P>
                     </tbody>
                 </table>
 
