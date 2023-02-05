d45637b8f6ef4ba6e02422e81c7a2e635ed7d2ae
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-05 21:45:27 +0330

    added project

---
+++ src/components/BookList/Book.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Book extends Component {
+  render() {
+    return (
+      <>
+    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Year</th>
+                        </tr>
+                    </thead>
+      
+      </>
+    )
+  }
+}
