8685e8ad40c8943ec2ef93e90c7f36ae78408979
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-05 22:13:04 +0330

    adde dproject

--- src/components/BookList/Booklist.jsx
+++ src/components/BookList/Booklist.jsx
@@ -77,7 +77,7 @@ g
                     </thead>
                     <tbody id="book-list">
                       <book/>
-                   <p></p>
+                   <p>mohammadjalzi</p>
                     </tbody>
                 </table>
 
